d18e2b83d27c3fa2b38d5b155ea30a7c60e3aef7
Liza Usova <Liza.usova@gmail.com>
2025-05-26 00:57:35 +0300

    hw_2 completed

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Card extends React.Component {
+    render() {
+    const {title, text} = this.props;
+
+        return <div className="card m-2">
+            <div className="card-body">
+                <h4 className="card-title">{title}</h4>
+                <p className="card-text">{text}</p>
+            </div>
+        </div>
+    }
+}
+export default Card
